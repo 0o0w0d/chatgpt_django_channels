a56bf5f81309b871d7443f7ee8b4d48d7709c671
zion <0o0w0d@gmail.com>
2024-05-16 01:54:29 +0900

    register admin: RolePlayingRoom model - using RolePlayingRoomForm - override save_model method

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import RolePlayingRoom
+from .forms import RolePlayingRoomForm
 
 # Register your models here.
+
+
+@admin.register(RolePlayingRoom)
+class RolePlayingRoomAdmin(admin.ModelAdmin):
+    form = RolePlayingRoomForm
+
+    def save_model(self, request, obj, form, change):
+        # 신규 생성 시에만 request.user를 할당
+        if change is False and form.is_valid():
+            obj.user = request.user
+
+        super().save_model(request, obj, form, change)
