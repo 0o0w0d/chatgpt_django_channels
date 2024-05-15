f045f9479f439b3694e0901e242aa1bee4677aec
zion <0o0w0d@gmail.com>
2024-05-15 20:28:10 +0900

    openai api test - use envfile - use Completion, ChatCompletion

---
+++ test_openai_api.py
@@ -0,0 +1,36 @@
+import os
+import openai
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+    engine="gpt-3.5-turbo-instruct",
+    prompt="""
+    Fix grammar errors:
+    - I is a boy
+    - You is a girls""".strip(),
+)
+
+print("test #1")
+print(response)
+print("응답 ::", response.choices[0].text.strip())
+
+print("--------------------------------")
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "당신은 지식이 풍부한 도우미입니다."},
+        {"role": "user", "content": "세계에서 가장 큰 도시는 어디인가요?"},
+    ],
+)
+
+print("test #2")
+print(response)
+print("응답 ::", response["choices"][0]["message"]["content"])
